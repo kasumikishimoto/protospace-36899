4369eb20a4820130d1932a778655acde1699c2a6
kasumikishimoto <kasumikishimot00@gmail.com>
2021-09-23 18:32:35 +0900

    protospace　修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :search]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -27,16 +27,14 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user.id == @prototype.user_id
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    prototype.update(prototype_params)
-     if prototype.save
-       redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+     if @prototype.save
+       redirect_to prototype_path
      else
        render :edit
      end
